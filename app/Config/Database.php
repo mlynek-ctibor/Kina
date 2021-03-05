fbca2b6424de6f46571d73f1da407663dc821d41
mlynek-ctibor <72254176+mlynek-ctibor@users.noreply.github.com>
2021-03-05 10:39:44 +0100

    oprava

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,7 +33,7 @@ class Database extends Config
 	public $default = [
 		'DSN'      => '',
 		'hostname' => 'localhost',
-		'username' => 'root',
+		'username' => '',
 		'password' => '',
 		'database' => '',
 		'DBDriver' => 'MySQLi',
